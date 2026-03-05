fbee3374853f047516009a1c844802c644f8b46a
ishtiaqa1 <ishtiaqa2003@gmail.com>
2026-03-05 16:19:12 -0500

    fixing cors issues

--- server/db.php
+++ server/db.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $servername = "sql210.infinityfree.com";
 $username = "if0_41315567";
 $password = "YOUR_PASSWORD";
