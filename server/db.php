e5ed1bfe914d5298b3f34bb7a78375260510f380
ishtiaq <ishtiaqa@buffalo.edu>
2026-03-06 00:13:59 -0500

    updated database

--- server/db.php
+++ server/db.php
@@ -8,12 +8,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-$servername = "sql210.infinityfree.com";
-$username = "if0_41315567";
-$password = "2n464RikknH2Ui";
-$dbname = "if0_41315567_Mahjong";
+$servername = "gondola.proxy.rlwy.net";
+$username = "root";
+$password = "JQDGkeFcCuDvmawPXRmlpTPvIOXxzqMR";
+$dbname = "railway";
+$port = 30830;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die(json_encode(["success" => false, "message" => "Connection failed: " . $conn->connect_error]));
