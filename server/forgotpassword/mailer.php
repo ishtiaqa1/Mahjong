091b29584aea0a2179f32dffd42bbbd2c27a213f
ishtiaq <ishtiaqa@buffalo.edu>
2026-03-06 14:15:41 -0500

    changhed mailer import

--- server/forgotpassword/mailer.php
+++ server/forgotpassword/mailer.php
@@ -17,7 +17,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 
-require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
     
 $mail = new PHPMailer(true);
 
